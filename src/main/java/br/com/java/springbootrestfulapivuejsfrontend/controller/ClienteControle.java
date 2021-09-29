a09851b5d2ff686096c182a7a3ffaf3568766d2c
Diego <diegoteixeirapereira@hotmail.com>
2021-09-29 09:25:07 -0300

    alteração no arquivo classe controle adicionado metodos crud.

--- src/main/java/br/com/java/springbootrestfulapivuejsfrontend/controller/ClienteControle.java
+++ src/main/java/br/com/java/springbootrestfulapivuejsfrontend/controller/ClienteControle.java
@@ -18,6 +18,7 @@ public class ClienteControle {
     @Autowired
     private ClienteRepositorio clienteRepositorio;
 
+    // Buscar Todos os Clientes
     @GetMapping("/clientes")
     public ResponseEntity<Object> buscarTodosClientes() {
         try {
@@ -28,6 +29,68 @@ public class ClienteControle {
             logger.error(e.getMessage(), e);
 
             return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Buscar Clientes por Id
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<Object> buscarClientePorId(@PathVariable("id") Long id) {
+        try {
+            Cliente cliente = clienteRepositorio.findById(id).get();
+
+            if (cliente != null) {
+                return new ResponseEntity<Object>(cliente, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Atualizar Dados do Cliente 
+    @PutMapping("/clientes/{id}")
+    public ResponseEntity<Object> atualizarCliente(@PathVariable("id")Long id, @RequestBody Cliente cliente) {
+        try {
+            cliente.setId(id);
+            Cliente salvarCliente = clienteRepositorio.save(cliente);
+
+            return new ResponseEntity<Object>(salvarCliente, HttpStatus.OK);
+
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Salvar Dados do Cliente
+    @PostMapping("/clientes")
+    public ResponseEntity<Object> criarCliente(@RequestBody Cliente cliente) {
+        try {
+            Cliente salvarCliente = clienteRepositorio.save(cliente);
+
+            return new ResponseEntity<Object>(salvarCliente, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Deletar Dados do Cliente 
+    @DeleteMapping("/clientes/{id}")
+    public ResponseEntity<HttpStatus> deletarCliente(@PathVariable("id")Long id) {
+        try {
+            clienteRepositorio.deleteById(id);
+
+            return new ResponseEntity<HttpStatus>(HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            
+            return new ResponseEntity<HttpStatus>(HttpStatus.BAD_REQUEST);
         }
     }
 }
